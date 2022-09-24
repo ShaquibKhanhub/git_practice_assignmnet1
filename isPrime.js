99dd5937053886d9afa8350efeaa1d6efff689b5
Abhijeet Hiwale <abhijeethiwalemail@gmail.com>
2022-09-24 17:51:12 +0530

    code fixed

--- isPrime.js
+++ isPrime.js
@@ -1,12 +1,15 @@
   
 
       let factor =0;
+
       for(let i =0;i<=num;i++){
-          if (num%i===1){
+          if (num%i === 0){
             factor++
           }
+
       }if(factor ==2){
           console.log("Yes")
       }else{
           console.log("No")
       }
+
